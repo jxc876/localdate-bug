cac01ab74577a2945b76515d5ed0f292bf4a83d2
Michael Ramirez <Michael.Ramirez@target.com>
2023-10-01 14:21:26 -0500

    store date as string, add converter.

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -1,11 +1,16 @@
 package com.example
 
+import io.micronaut.core.convert.ConversionContext
 import io.micronaut.data.annotation.Id
 import io.micronaut.data.annotation.MappedEntity
+import io.micronaut.data.annotation.TypeDef
 import io.micronaut.data.jdbc.annotation.JdbcRepository
+import io.micronaut.data.model.DataType
 import io.micronaut.data.model.query.builder.sql.Dialect
+import io.micronaut.data.model.runtime.convert.AttributeConverter
 import io.micronaut.data.repository.CrudRepository
 import io.micronaut.runtime.Micronaut.run
+import jakarta.inject.Singleton
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 import java.util.*
@@ -17,16 +22,27 @@ val date: LocalDate = LocalDate.parse("2023-09-28", DateTimeFormatter.ISO_LOCAL_
 @MappedEntity("event")
 data class Event(
 	@field:Id val id: UUID,
+
+	@field:TypeDef(type = DataType.STRING, converter = LocalDateConverter::class)
 	val date: LocalDate
 )
 
 @JdbcRepository(dialect = Dialect.POSTGRES)
-interface EventRepository: CrudRepository<Event, UUID> {}
+interface EventRepository: CrudRepository<Event, UUID>
 
-/**
- * Saves an object to database and read its back.
- * BUG: LocalDate is always saved incorrectly inside the DB (off by 1 day)
- */
+@Singleton
+class LocalDateConverter: AttributeConverter<LocalDate, String> {
+	private val formatter: DateTimeFormatter = DateTimeFormatter.ISO_LOCAL_DATE
+	override fun convertToPersistedValue(entityValue: LocalDate?, context: ConversionContext): String? {
+		return formatter.format(entityValue)
+	}
+
+	override fun convertToEntityValue(persistedValue: String?, context: ConversionContext): LocalDate? {
+		return LocalDate.parse(persistedValue, formatter)
+	}
+
+}
+
 fun main(args: Array<String>) {
 	val context = run(*args)
 	val repo = context.getBean(EventRepository::class.java)
